3d8a8fdad6cc9cf6988f941df3fe720089f69517
skoulouzis <S.Koulouzis@uva.nl>
2018-09-26 16:56:48 +0300

    get mapping from conf

--- rest_cat/src/main/resources/public/check_input.js
+++ rest_cat/src/main/resources/public/check_input.js
@@ -1,7 +1,10 @@
 function checkInputs() {
     var catalogueURL = document.getElementById("cat_url").value;
     var targetSelect = document.getElementById("target").value;
-    console.log(targetInt);
+
+    var startButt = document.getElementById("startBtn");
+    startButt.disabled = true;
+
     var targetInt = parseInt(targetSelect)
     switch (targetInt) {
         case 1:
@@ -9,49 +12,37 @@ function checkInputs() {
             if (mappingSelect.options.length < 2) {
                 var option = document.createElement("option");
                 option.text = "Mapping115";
+                option.value = 1;
+                mappingSelect.add(option);
+                option = document.createElement("option");
+                option.text = "Mapping120";
+                option.value = 2;
+                mappingSelect.add(option);
+                option = document.createElement("option");
+                option.text = "Mapping62";
+                option.value = 3;
                 mappingSelect.add(option);
             }
             break;
 
         default:
             var mappingSelect = document.getElementById("mappingSelect");
+
             if (mappingSelect.options.length >= 2) {
                 mappingSelect.remove(mappingSelect.selectedIndex);
             }
-
             break;
     }
+    var mappingSelect = document.getElementById("mappingSelect").value;
+    var mappingInt = parseInt(mappingSelect)
+    console.log(mappingInt);
 
 
-
-
-
-
-//        console.log(mapping115);
-//        if (mapping115 !== null) {
-//            var option = document.createElement("option");
-//            option.text = "Mapping115";
-//            mappingSelect.add(option);
-//        }
-
-
-
-
-
-//    console.log(catalogueURL);
-//    console.log(targetInt);
-    if (catalogueURL !== null && targetInt > 0) {
-        var startButt = document.getElementById("startBtn");
+    if (catalogueURL !== null && targetInt >= 1 && mappingInt >= 1) {
         startButt.disabled = false;
+        return true;
+    } else {
+        startButt.disabled = true;
+        return false;
     }
-}
-
-
-function configure() {
-// Plain JavaScript
-    fileSelector.setAttribute('multiple', 'multiple');
-
-// jQuery - change the file select
-    var fileSelector = $('<input type="file" multiple="">');
-
 }
