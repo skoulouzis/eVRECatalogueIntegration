5450c6bc0ac0ecf07835ec54572f4a6105742244
skoulouzis <S.Koulouzis@uva.nl>
2018-10-08 14:21:00 +0200

    change prov doc to fit model

--- LOG2PROV/provenance/log_prov.py
+++ LOG2PROV/provenance/log_prov.py
@@ -3,46 +3,26 @@ from prov.model import ProvDocument, Namespace, Literal, PROV, Identifier
 class LogProv():
     def __init__(self,log_dic):        
         self._prov_doc = ProvDocument()
-        self._prov_doc.add_namespace('remote_host', 'https://www.vre4eic.eu/remote_host/#')
-        remote_host = self._prov_doc.agent('remote_host:'+log_dic['remote_host'])
-        
-        if('request_url_hostname' in log_dic and log_dic['request_url_hostname']):
-            self._prov_doc.add_namespace('request_hostname', 'https://www.vre4eic.eu/request_hostname/#')
-            request_hostname= self._prov_doc.agent('request_hostname:'+log_dic['request_url_hostname'])
-
-        self._prov_doc.add_namespace('response_bytes', 'https://www.vre4eic.eu/response_bytes/#')
-        response_bytes = self._prov_doc.entity('response_bytes:'+log_dic['response_bytes_clf'])
-
-        self._prov_doc.add_namespace('http_status', 'https://www.vre4eic.eu/http_status/#')
-        http_status = self._prov_doc.entity('http_status:'+log_dic['status'])
-
+        vre_namespace = self._prov_doc.add_namespace('vre', 'https://www.vre4eic.eu/log#')
+        prov_namespace = self._prov_doc.add_namespace('prov', 'http://www.w3.org/ns/prov#')
         if('request_url_username' in log_dic and log_dic['request_url_username']):
-            self._prov_doc.add_namespace('request_url_username', 'https://www.vre4eic.eu/request_url_username/#')
-            request_url_username = self._prov_doc.entity('request_url_username:'+log_dic['request_url_username'])
+            remote_host = self._prov_doc.agent(vre_namespace['ag1'], {prov_namespace['type']: PROV["SoftwareAgent"], vre_namespace['hasIP']: log_dic['remote_host'],vre_namespace['hasUsername']:log_dic['request_url_username']})    
+        else:
+            remote_host = self._prov_doc.agent(vre_namespace['ag1'], {prov_namespace['type']: PROV["SoftwareAgent"], vre_namespace['hasIP']: log_dic['remote_host']})
+        
+        if('request_url_hostname' in log_dic and log_dic['request_url_hostname']):            
+            request_hostname = self._prov_doc.agent(vre_namespace['ag2'], {
+            prov_namespace['type']: PROV["SoftwareAgent"], vre_namespace['hasIP']: log_dic['remote_host']
+            })
+        
+        
+        request_entity = self._prov_doc.entity(vre_namespace['en1'], {vre_namespace['status']: log_dic['status'],vre_namespace['responseBytes']:log_dic['response_bytes_clf']})
 
-        self._prov_doc.add_namespace('request_url', 'https://www.vre4eic.eu/request_url/#')
-        request_url = self._prov_doc.entity('request_url:'+log_dic['request_url'])
+        received_activity = self._prov_doc.activity(vre_namespace['ac1'], other_attributes={vre_namespace['requestURL']:log_dic['request_url'],vre_namespace['requestMethod']:log_dic['request_method'], vre_namespace['httpVersion']:log_dic['request_http_ver']})        
+        self._prov_doc.generation(remote_host,activity=received_activity,time=log_dic['time_received_tz_isoformat'])
+        self._prov_doc.wasAttributedTo(request_entity, received_activity)
+        self._prov_doc.wasAssociatedWith(received_activity, remote_host)
 
-        self._prov_doc.add_namespace('http_staus_code', 'https://www.vre4eic.eu/http_staus_code/#')
-        http_staus_code = self._prov_doc.entity('http_staus_code:'+log_dic['status'])
-
-        self._prov_doc.add_namespace('request_method', 'https://www.vre4eic.eu/request_method/#')
-        request_method = self._prov_doc.entity('request_method:'+log_dic['request_method'])
-
-        self._prov_doc.add_namespace('request_http_ver', 'https://www.vre4eic.eu/request_http_ver/#')
-        request_http_ver = self._prov_doc.entity('request_http_ver:'+log_dic['request_http_ver'])
-
-
-        self._prov_doc.add_namespace('is', 'https://www.vre4eic.eu/is#')
-        received_activity = self._prov_doc.activity('is:received')
-
-    #    self._prov_doc.add_namespace('logger', 'https://www.vre4eic.eu/logger/#')
-    #    logger = self._prov_doc.entity('logger:catalina_logger')
-        self._prov_doc.generation(remote_host,activity=received_activity,time='2018-09-13T12:51:13+00:00')
-
-
-    #    self._prov_doc.add_namespace('is', 'https://www.vre4eic.eu/is#')
-    #    response_activity = self._prov_doc.activity('is:response')    
     
     @property
     def prov_doc(self):
