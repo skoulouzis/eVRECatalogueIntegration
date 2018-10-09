c0d5e74290d2f862da7e683bd85815f4a9f4f86d
skoulouzis <S.Koulouzis@uva.nl>
2018-10-09 13:45:52 +0200

    convert binding class to string

--- LOG2PROV/provenance/log_binding.py
+++ LOG2PROV/provenance/log_binding.py
@@ -4,9 +4,9 @@ from .binding_var import *
 
 class LogBinding():
     def __init__(self,log_dic):
-        self._binding = {}
-        namespaces = [{'prov':'http://www.w3.org/ns/prov#'},{'tmpl':'http://openprovenance.org/tmpl#'},{'var':'http://openprovenance.org/var#'},{'vre','https://www.vre4eic.eu/log#'}]
-        self._binding['namespaces'] = namespaces
+        self._binding_dict = {}
+        namespaces = [{'prov':'http://www.w3.org/ns/prov#'},{'tmpl':'http://openprovenance.org/tmpl#'},{'var':'http://openprovenance.org/var#'},{'vre':'https://www.vre4eic.eu/log#'}]
+        self._binding_dict['namespaces'] = namespaces
         
         vars = []
         if('request_url_username' in log_dic and log_dic['request_url_username']):
@@ -20,20 +20,30 @@ class LogBinding():
         vars.append(LogVar('version','Entity','2dvalue_0_0', log_dic['request_http_ver']))
         vars.append(LogVar('time','Entity','value_0', log_dic['time_received_tz_isoformat']))
         
-        self._binding['vars'] = vars
+        self._binding_dict['vars'] = vars
         
-
-        print(self._binding)
 
     
     @property
-    def binding(self):
-        return self._binding
+    def binding_dict(self):
+        return self._binding_dict
 
-    @binding.setter
-    def binding(self, value):
-        self._binding = value
+    @binding_dict.setter
+    def binding_dict(self, value):
+        self._binding_dict = value
 
-    @binding.deleter
-    def binding(self):
-        del self._binding
+    @binding_dict.deleter
+    def binding_dict(self):
+        del self._binding_dict
+        
+        
+    @property
+    def vars_str(self):
+        vars_str = ''
+        for var in  self._binding_dict['vars']:
+            vars_str+='var:'+var.var_name+' a prov:'+var.prov_type+' ;\n\t'+'tmpl:'+var.template_value_type+' "'+var.value+'" .\n'
+        return vars_str.rstrip()
+    
+    @property
+    def namespaces(self):
+        return self._binding_dict['namespaces']
