6183aff694cbafa2f0b617945e0af20ebe69eb2d
skoulouzis <S.Koulouzis@uva.nl>
2018-10-10 15:13:00 +0200

    fixed format. Added entity type

--- LOG2PROV/provenance/binding_var.py
+++ LOG2PROV/provenance/binding_var.py
@@ -1,57 +1,23 @@
 class LogVar():
-    def __init__(self, var_name, prov_type, template_value_type,value):
+    def __init__(self, var_name, prefix, value_type, value):
         self._var_name = var_name
-        self._prov_type = prov_type
-        self._template_value_type = template_value_type
+        self._prefix = prefix
+        self._value_type = value_type
         self._value = value;
-        
-
     
     @property
     def var_name(self):
         return self._var_name
-
-    @var_name.setter
-    def var_name(self, value):
-        self._var_name = value
-
-    @var_name.deleter
-    def var_name(self):
-        del self._var_name
         
     @property
-    def prov_type(self):
-        return self._prov_type
-
-    @prov_type.setter
-    def prov_type(self, value):
-        self._prov_type = value
-
-    @prov_type.deleter
-    def prov_type(self):
-        del self._prov_type        
+    def prefix(self):
+        return self._prefix   
         
     @property
-    def template_value_type(self):
-        return self._template_value_type
-
-    @template_value_type.setter
-    def template_value_type(self, value):
-        self._template_value_type = value
-
-    @template_value_type.deleter
-    def template_value_type(self):
-        del self._template_value_type      
+    def value_type(self):
+        return self._value_type    
         
         
     @property
     def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, value):
-        self._value = value
-
-    @value.deleter
-    def value(self):
-        del self._value           
+        return self._value     
--- LOG2PROV/provenance/log_binding.py
+++ LOG2PROV/provenance/log_binding.py
@@ -1,26 +1,37 @@
-from prov.model import ProvDocument, Namespace, Literal, PROV, Identifier
 from .binding_var import *
+from .entity_type import *
 
 
 class LogBinding():
-    def __init__(self,log_dic):
+    def __init__(self, log_dic, value_index):
         self._binding_dict = {}
-        namespaces = [{'prov':'http://www.w3.org/ns/prov#'},{'tmpl':'http://openprovenance.org/tmpl#'},{'var':'http://openprovenance.org/var#'},{'vre':'https://www.vre4eic.eu/log#'}]
+        namespaces = [{'prov':'http://www.w3.org/ns/prov#'}, {'tmpl':'http://openprovenance.org/tmpl#'}, {'var':'http://openprovenance.org/var#'}, {'vre':'https://www.vre4eic.eu/log#'}]
         self._binding_dict['namespaces'] = namespaces
-        
-        vars = []
+        entity_types = []
+        binding_vars = []
         if('request_url_username' in log_dic and log_dic['request_url_username']):
-            vars.append(LogVar('request_url_username','Entity','2dvalue_0_0', log_dic['request_url_username']))
-
-        vars.append(LogVar('ip','Entity','2dvalue_0_0', log_dic['remote_host']))
-        vars.append(LogVar('status','Entity','2dvalue_0_0', log_dic['status']))
-        vars.append(LogVar('bytes','Entity','2dvalue_0_0', log_dic['response_bytes_clf']))
-        vars.append(LogVar('url','Entity','2dvalue_0_0', log_dic['request_url']))
-        vars.append(LogVar('method','Entity','2dvalue_0_0', log_dic['request_method']))
-        vars.append(LogVar('version','Entity','2dvalue_0_0', log_dic['request_http_ver']))
-        vars.append(LogVar('time','Entity','value_0', log_dic['time_received_tz_isoformat']))
+            binding_vars.append(LogVar('request_url_username', 'tmpl', '2dvalue_' + str(value_index) + '_0', log_dic['request_url_username']))
+            entity_types.append(EntityType('request_url_username','prov','Entity'))
+            
+        binding_vars.append(LogVar('agent', 'tmpl', 'value_' + str(value_index), 'vre:ag'+str(value_index)))
+        entity_types.append(EntityType('agent','prov','Entity'))
+        binding_vars.append(LogVar('ip', 'tmpl', '2dvalue_' + str(value_index) + '_0', log_dic['remote_host']))
+        entity_types.append(EntityType('ip','prov','Entity'))
+        binding_vars.append(LogVar('status', 'tmpl', '2dvalue_' + str(value_index) + '_0', log_dic['status']))
+        entity_types.append(EntityType('status','prov','Entity'))
+        binding_vars.append(LogVar('bytes', 'tmpl', '2dvalue_' + str(value_index) + '_0', log_dic['response_bytes_clf']))
+        entity_types.append(EntityType('bytes','prov','Entity'))
+        binding_vars.append(LogVar('url', 'tmpl', '2dvalue_' + str(value_index) + '_0', log_dic['request_url']))
+        entity_types.append(EntityType('url','prov','Entity'))
+        binding_vars.append(LogVar('method', 'tmpl', '2dvalue_' + str(value_index) + '_0', log_dic['request_method']))
+        entity_types.append(EntityType('method','prov','Entity'))
+        binding_vars.append(LogVar('version', 'tmpl', '2dvalue_' + str(value_index) + '_0', log_dic['request_http_ver']))
+        entity_types.append(EntityType('version','prov','Entity'))
+        binding_vars.append(LogVar('time', 'tmpl', 'value_' + str(value_index), log_dic['time_received_tz_isoformat']))
+        entity_types.append(EntityType('time','prov','Entity'))
         
-        self._binding_dict['vars'] = vars
+        self._binding_dict['vars'] = binding_vars
+        self._binding_dict['entity_types'] = entity_types
         
 
     
@@ -35,15 +46,15 @@ class LogBinding():
     @binding_dict.deleter
     def binding_dict(self):
         del self._binding_dict
-        
-        
+            
     @property
-    def vars_str(self):
-        vars_str = ''
-        for var in  self._binding_dict['vars']:
-            vars_str+='var:'+var.var_name+' a prov:'+var.prov_type+' ;\n\t'+'tmpl:'+var.template_value_type+' "'+var.value+'" .\n'
-        return vars_str.rstrip()
+    def binding_vars(self):
+        return self._binding_dict['vars'] 
     
     @property
     def namespaces(self):
         return self._binding_dict['namespaces']
+    
+    @property
+    def entity_types(self):
+        return self._binding_dict['entity_types']    
