5fa7128a1d13c696512c1153f5382e6412f5a087
skoulouzis <S.Koulouzis@uva.nl>
2018-11-23 13:33:50 +0100

    change backup script

--- CatMap/docker/backup_influx.sh
+++ CatMap/docker/backup_influx.sh
@@ -5,3 +5,4 @@ influx_id=`sudo docker ps -aqf "name=influx"`
 sudo docker exec  -ti $influx_id influxd backup -portable -database mydb /tmp/mydb
 sudo docker cp $influx_id:/tmp/mydb /tmp/
 sudo chown -R $USER /tmp/mydb
+mv /tmp/mydb /tmp/mydb_$HOSTNAME
