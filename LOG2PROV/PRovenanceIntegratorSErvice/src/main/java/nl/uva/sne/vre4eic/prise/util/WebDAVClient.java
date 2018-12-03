262fdf900146163c1341ef775cc1ee8dd3a1508c
skoulouzis <S.Koulouzis@uva.nl>
2018-12-03 17:32:14 +0100

    fixed webdav bug

--- LOG2PROV/PRovenanceIntegratorSErvice/src/main/java/nl/uva/sne/vre4eic/prise/util/WebDAVClient.java
+++ LOG2PROV/PRovenanceIntegratorSErvice/src/main/java/nl/uva/sne/vre4eic/prise/util/WebDAVClient.java
@@ -9,29 +9,11 @@ package nl.uva.sne.vre4eic.prise.util;
  *
  * @author alogo
  */
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.TreeMap;
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.FileRequestEntity;
-import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
 import org.apache.commons.httpclient.methods.PutMethod;
 import org.apache.commons.httpclient.methods.RequestEntity;
-
-import org.apache.jackrabbit.webdav.DavConstants;
-import org.apache.jackrabbit.webdav.MultiStatus;
-import org.apache.jackrabbit.webdav.MultiStatusResponse;
-import org.apache.jackrabbit.webdav.property.DavProperty;
-import org.apache.jackrabbit.webdav.property.DavPropertySet;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import static org.springframework.http.RequestEntity.method;
 
 public class WebDAVClient {
 
@@ -44,7 +26,7 @@ public class WebDAVClient {
     }
 
     public int putFile(File workflowFile, String webdavFolder, String mimetype) throws Exception {
-        PutMethod httpMethod = new PutMethod("http://localhost/" + webdavFolder + "/" + workflowFile.getName());
+        PutMethod httpMethod = new PutMethod(uri + webdavFolder + "/" + workflowFile.getName());
         int statusCode;
         RequestEntity requestEntity = new FileRequestEntity(workflowFile, mimetype);
         httpMethod.setRequestEntity(requestEntity);
