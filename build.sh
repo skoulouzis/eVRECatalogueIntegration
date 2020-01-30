a7923c24bddfef398b473b1b46007cf33567dd38
skoulouzis <S.Koulouzis@uva.nl>
2020-01-30 14:06:08 +0100

    added repo

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-mvn install 
-
-cd cat_worker/
-
-mvn clean compile assembly:single
+mvn install  &&  cd cat_worker/ && mvn compile assembly:single
 cp target/cat_worker-1.0-SNAPSHOT-jar-with-dependencies.jar ../docker/cat_worker/
 
 
